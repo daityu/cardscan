df1eb9e0ce72af8cb9f5ae50252c8b94c946c3e3
oonaka <k_oonaka@n-c-c.co.jp>
2024-03-01 09:55:20 +0900

    整理

--- demo.bat
+++ demo.bat
@@ -3,4 +3,3 @@ set HTTPS_PROXY=http://192.168.100.253:8080/
 set HTTP_PROXY=http://192.168.100.253:8080/
 set Path=C:\PHP82_64;%Path%
 C:\PHP82_64\php.exe -S localhost:8890 -t C:\git\cardscan\docs
-rem C:\PHP74_64\php.exe -S 192.168.10.105:8889 -t C:\git\kaifcloud_dev\public
